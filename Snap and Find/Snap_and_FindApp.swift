423dad84c3a7afb2510f2d87f235b0a8bc6de030
Daniel Larsen <daniel.larsen@bottlerocketstudios.com>
2022-10-20 10:06:23 -0500

    Initial Commit

---
+++ Snap and Find/Snap_and_FindApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snap_and_FindApp.swift
+//  Snap and Find
+//
+//  Created by Daniel Larsen on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Snap_and_FindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
